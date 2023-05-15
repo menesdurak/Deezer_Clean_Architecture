b9b2f370e51931d8d3ae84c31677576d2c59fef8
menesdurak <m.enes.durak@gmail.com>
2023-05-15 13:31:15 +0300

    Changed Hilt app

---
+++ app/src/main/java/com/menesdurak/appcentcasestudycleanarchitecture/HiltApplication.kt
@@ -0,0 +1,7 @@
+package com.menesdurak.appcentcasestudycleanarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application()
