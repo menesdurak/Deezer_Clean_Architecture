4121c57609aa3b237855296db0f0744a3e9c7cb3
menesdurak <m.enes.durak@gmail.com>
2023-05-15 18:26:32 +0300

    Created tracks fragment

---
+++ app/src/main/java/com/menesdurak/appcentcasestudycleanarchitecture/presentation/tracks/TracksFragment.kt
@@ -0,0 +1,28 @@
+package com.menesdurak.appcentcasestudycleanarchitecture.presentation.tracks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.menesdurak.appcentcasestudycleanarchitecture.databinding.FragmentTracksBinding
+
+class TracksFragment : Fragment() {
+    private var _binding: FragmentTracksBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTracksBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
