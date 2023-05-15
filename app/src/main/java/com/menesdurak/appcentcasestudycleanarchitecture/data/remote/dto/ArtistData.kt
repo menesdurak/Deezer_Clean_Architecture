f28df11804bc57f9a38a289e28044e4b22a40b8a
menesdurak <m.enes.durak@gmail.com>
2023-05-15 13:37:03 +0300

    Created artist dto

---
+++ app/src/main/java/com/menesdurak/appcentcasestudycleanarchitecture/data/remote/dto/ArtistData.kt
@@ -0,0 +1,14 @@
+package com.menesdurak.appcentcasestudycleanarchitecture.data.remote.dto
+
+data class ArtistData(
+    val id: Int,
+    val name: String,
+    val picture: String,
+    val picture_big: String,
+    val picture_medium: String,
+    val picture_small: String,
+    val picture_xl: String,
+    val radio: Boolean,
+    val tracklist: String,
+    val type: String
+)
---
+++ app/src/main/java/com/menesdurak/appcentcasestudycleanarchitecture/data/remote/dto/ArtistResponse.kt
@@ -0,0 +1,5 @@
+package com.menesdurak.appcentcasestudycleanarchitecture.data.remote.dto
+
+data class ArtistResponse(
+    val data: List<ArtistData>
+)
