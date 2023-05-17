dded5d7cf0dbc21cc76185030db3bb78ca0d8b21
menesdurak <m.enes.durak@gmail.com>
2023-05-17 20:33:32 +0300

    Created dao

---
+++ app/src/main/java/com/menesdurak/appcentcasestudycleanarchitecture/data/local/DeezerDao.kt
@@ -0,0 +1,20 @@
+package com.menesdurak.appcentcasestudycleanarchitecture.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.menesdurak.appcentcasestudycleanarchitecture.data.local.entity.FavoriteTrack
+
+@Dao
+interface DeezerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavoriteTrack(favoriteTrack: FavoriteTrack)
+
+    @Query("DELETE FROM deezer_table WHERE id = :favoriteTrackId")
+    suspend fun deleteFavoriteTrackWithId(favoriteTrackId: Long)
+
+    @Query("SELECT * FROM deezer_table ORDER BY id ASC")
+    suspend fun getAllFavoriteTracks(): List<FavoriteTrack>
+}
